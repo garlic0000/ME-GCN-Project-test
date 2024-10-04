f81c34cb2a65c13addb57a4e59625fb210fd8e30
garlic0000 <garlic0000@163.com>
2024-10-04 11:57:11 +0800

    Update opencv_cmake.sh

--- other/opencv_cmake.sh
+++ other/opencv_cmake.sh
@@ -38,7 +38,7 @@ cmake \
     -DPYTHON_DEFAULT_EXECUTABLE=/opt/conda/bin/python3 \
     -DPYTHON_INCLUDE_DIR=/opt/conda/include/python3.10 \
     -DPYTHON_LIBRARY=/opt/conda/lib/libpython3.10.so \
-    -DPYTHONPATH=/usr/local/opencv-4.10/python/cv2 \
+    -DPYTHON_INSTALL_DIR=/usr/local/opencv-4.10/python/cv2 \
     -DWITH_OPENGL=OFF \
     -DWITH_VTK=OFF \
     -DFORCE_VTK=OFF \
