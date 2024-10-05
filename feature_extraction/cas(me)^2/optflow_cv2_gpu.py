fb750035d4c1796a8edfef5712c5ae680b66a81f
garlic0000 <garlic0000@163.com>
2024-10-05 09:37:34 +0800

    更新

--- feature_extraction/cas(me)^2/optflow_cv2_gpu.py
+++ feature_extraction/cas(me)^2/optflow_cv2_gpu.py
@@ -95,7 +95,8 @@ def optflow(opt):
 
 
 if __name__ == "__main__":
-    with open("/kaggle/working/ME-GCN-Project/feature_extraction/cas(me)^2/config.yaml", encoding="UTF-8") as f:
+    with open("/kaggle/working/ME-GCN-Project/feature_extraction/cas(me)^2/config_test.yaml", encoding="UTF-8") as f:
+    # with open("/kaggle/working/ME-GCN-Project/feature_extraction/cas(me)^2/config.yaml", encoding="UTF-8") as f:
         yaml_config = yaml.safe_load(f)
         dataset = yaml_config['dataset']
         opt = yaml_config[dataset]
